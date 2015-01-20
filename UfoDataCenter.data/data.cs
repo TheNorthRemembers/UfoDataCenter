86ddd22b451eb9cb819d5c7d609e9397be7505f7
Michael Bondi <mbondi@rls2000.com>
2015-01-19 21:34:57 -0500

    Up to date

--- UfoDataCenter.data/data.cs
+++ UfoDataCenter.data/data.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UfoDB;
 namespace UfoDataCenter
 {
     public class data
     {
+        
     }
 }
